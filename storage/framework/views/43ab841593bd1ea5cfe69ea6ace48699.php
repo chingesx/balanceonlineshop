52c14b5bd2b95a39547747ae7396badca3f7f3c3
chingesx <117958951+chingesx@users.noreply.github.com>
2023-11-15 22:46:21 +0800

    Add files via upload

---
+++ storage/framework/views/43ab841593bd1ea5cfe69ea6ace48699.php
@@ -0,0 +1,40 @@
+
+ 
+<?php $__env->startSection('content'); ?>
+  <div class="container-fluid py-4">
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <div class="card">
+          <div class="card-body p-3">
+            <h5>Add Product</h5> 
+            <hr>
+            <form action="<?php echo e(route('product.store')); ?>" method="POST" enctype="multipart/form-data">
+              <?php echo csrf_field(); ?>
+              <div class="form-group">
+                <label for="name" class="form-control-label">Name</label>
+                <input type="text" name="name" class="form-control" id="name" required>
+              </div>
+              <div class="form-group">
+                <label for="image" class="form-control-label">Image</label>
+                <input type="file" name="image" class="form-control form-control-file" id="image" accept=".jpg, .png, .svg" required>
+              </div>
+              <div class="form-group">
+                <label for="description" class="form-control-label">Description</label>
+                <textarea rows="6" name="description" class="form-control" id="description" required></textarea>
+              </div>
+              <div class="form-group">
+                <label for="price" class="form-control-label">Price</label>
+                <input type="number" name="price" class="form-control" id="price" required>
+                <small>* Minimal $. 1</small>
+              </div>
+              <div class="form-group">
+                <button class="btn btn-primary">Add</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\hicheel\4-1\ogodol security\biy-daalt\shop\resources\views/pages/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/6773f4cae424a85b195d9ef603d48fb2.php
@@ -0,0 +1,164 @@
+
+ 
+<?php $__env->startSection('content'); ?>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Profile</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">User Profile</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+ 
+    <!-- Main content -->
+    <section class="content">
+    <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+ 
+            <!-- Profile Image -->
+            <div class="card card-primary card-outline">
+              <div class="card-body box-profile">
+                <div class="text-center">
+                  <img class="profile-user-img img-fluid img-circle"
+                       src="../assets/img/avatar4.png"
+                       alt="User profile picture">
+                </div>
+ 
+                <h3 class="profile-username text-center"><?php echo e(Auth::user()->name); ?> - <?php echo e(Auth::user()->username); ?></h3>
+ 
+                <p class="text-muted text-center"><?php echo e(Auth::user()->email); ?></p>
+               </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+ 
+            <!-- About Me Box -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">About Me</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <strong><i class="fas fa-book mr-1"></i> Company</strong>
+ 
+                <p class="text-muted">
+                    Hi-tech
+            </p>
+ 
+                <hr>
+ 
+                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+ 
+                <p class="text-muted">Mongolia</p>
+ 
+                <hr>
+ 
+                <strong><i class="fas fa-pencil-alt mr-1"></i> Accuracy</strong>
+ 
+                <p class="text-muted">
+                  <span class="tag tag-danger">100%</span>
+                </p>
+ 
+                <hr>
+ 
+                <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+ 
+                <p class="text-muted">Bi sain hun ndd itgeed mni baraag av</p>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-9">
+            <div class="card">
+              <div class="card-header p-2">
+                <ul class="nav nav-pills">
+                  <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Activity</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li>
+                </ul>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content">
+                  <div class="active tab-pane" id="activity">
+                  activity
+                  </div>
+                  <!-- /.tab-pane -->
+                  <div class="tab-pane" id="timeline">
+                    timeline
+                  </div>
+                  <!-- /.tab-pane -->
+ 
+                  <div class="tab-pane" id="settings">
+                    <form class="form-horizontal">
+                      <div class="form-group row">
+                        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputName" placeholder="Name">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputName2" placeholder="Name">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
+                        <div class="col-sm-10">
+                          <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <div class="offset-sm-2 col-sm-10">
+                          <div class="checkbox">
+                            <label>
+                              <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <div class="offset-sm-2 col-sm-10">
+                          <button type="submit" class="btn btn-danger">Submit</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                  <!-- /.tab-pane -->
+                </div>
+                <!-- /.tab-content -->
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\hicheel\4-1\ogodol security\biy-daalt\shop\resources\views/pages/profile.blade.php ENDPATH**/ ?>
